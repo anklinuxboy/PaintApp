022969567952ddfad7f6bf707343e5df1b8b3d8e
Ankit Sharma <ankit@intrepid.io>
2016-09-20 19:26:12 -0400

    Click Listener for pallete and brush width

--- app/src/main/java/com/example/ankitsharma/paint/PaintFragment.java
+++ app/src/main/java/com/example/ankitsharma/paint/PaintFragment.java
@@ -3,6 +3,7 @@ package com.example.ankitsharma.paint;
 import android.app.Fragment;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -29,6 +30,33 @@ public class PaintFragment extends Fragment implements View.OnClickListener {
     @BindView(R.id.custom_paint_view)
     PaintView paintView;
 
+    // Pallete Buttons
+    @BindView(R.id.black_button)
+    ImageButton blackButton;
+    @BindView(R.id.blue_button)
+    ImageButton blueButton;
+    @BindView(R.id.red_button)
+    ImageButton redButton;
+    @BindView(R.id.green_button)
+    ImageButton greenButton;
+    @BindView(R.id.yellow_button)
+    ImageButton yellowButton;
+    @BindView(R.id.magenta_button)
+    ImageButton magentaButton;
+
+    // Brush Buttons
+    @BindView(R.id.brush_24)
+    ImageButton brush24;
+    @BindView(R.id.brush_18)
+    ImageButton brush18;
+    @BindView(R.id.brush_12)
+    ImageButton brush12;
+    @BindView(R.id.brush_6)
+    ImageButton brush6;
+
+    private boolean palleteVisible = false;
+    private boolean brushVisible = false;
+
     int RED = Color.RED;
     int GREEN = Color.GREEN;
     int YELLOW = Color.YELLOW;
@@ -44,12 +72,26 @@ public class PaintFragment extends Fragment implements View.OnClickListener {
         deleteButton.setOnClickListener(this);
         brushButton.setOnClickListener(this);
         palleteButton.setOnClickListener(this);
+        // Set up listeners on pallete
+        blackButton.setOnClickListener(this);
+        blueButton.setOnClickListener(this);
+        greenButton.setOnClickListener(this);
+        yellowButton.setOnClickListener(this);
+        magentaButton.setOnClickListener(this);
+        redButton.setOnClickListener(this);
+        // listeners for brush width
+        brush24.setOnClickListener(this);
+        brush18.setOnClickListener(this);
+        brush12.setOnClickListener(this);
+        brush6.setOnClickListener(this);
 
         return view;
     }
 
     @Nullable
-    @OnClick({R.id.delete_icon, R.id.brush_icon, R.id.pallete_icon})
+    @OnClick({R.id.delete_icon, R.id.brush_icon, R.id.pallete_icon,
+    R.id.black_button, R.id.blue_button, R.id.red_button, R.id.yellow_button, R.id.green_button,
+     R.id.magenta_button, R.id.brush_12, R.id.brush_24, R.id.brush_18, R.id.brush_6})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.delete_icon:
@@ -58,12 +100,50 @@ public class PaintFragment extends Fragment implements View.OnClickListener {
                 break;
             case R.id.brush_icon:
                 Timber.d("Brush Button Pressed");
+                setBrushWidthVisible();
                 break;
             case R.id.pallete_icon:
                 Timber.d("Pallete Button Pressed");
+                setPalleteVisible();
                 break;
             default:
                 return;
+        }
+    }
+
+    private void setBrushWidthVisible() {
+        if (brushVisible == false) {
+            brush24.setVisibility(View.VISIBLE);
+            brush18.setVisibility(View.VISIBLE);
+            brush12.setVisibility(View.VISIBLE);
+            brush6.setVisibility(View.VISIBLE);
+            brushVisible = true;
+        } else {
+            brush24.setVisibility(View.GONE);
+            brush18.setVisibility(View.GONE);
+            brush12.setVisibility(View.GONE);
+            brush6.setVisibility(View.GONE);
+            brushVisible = false;
+        }
+    }
+
+    private void setPalleteVisible() {
+        if (palleteVisible == false) {
+            blackButton.setVisibility(View.VISIBLE);
+            redButton.setVisibility(View.VISIBLE);
+            blueButton.setVisibility(View.VISIBLE);
+            greenButton.setVisibility(View.VISIBLE);
+            magentaButton.setVisibility(View.VISIBLE);
+            yellowButton.setVisibility(View.VISIBLE);
+            palleteVisible = true;
+        } else {
+            blackButton.setVisibility(View.GONE);
+            redButton.setVisibility(View.GONE);
+            blueButton.setVisibility(View.GONE);
+            greenButton.setVisibility(View.GONE);
+            magentaButton.setVisibility(View.GONE);
+            yellowButton.setVisibility(View.GONE);
+            palleteVisible = false;
         }
     }
 }
